c23395495182a6618fe2118529192d68fc2cd1bf
jagrutikpatil <113102208+jagrutikpatil@users.noreply.github.com>
2023-06-09 18:01:02 +0530

    Add files via upload

---
+++ Tribute Page/logic.js
@@ -0,0 +1,19 @@
+let x=function(e){
+    alert("Submitted")
+  }
+  
+  
+  btn1.addEventListener('click',function(a) {
+    alert("Submitted")
+  })
+  
+//   btn.addEventListener('click',function(a) {
+//     alert("Hello 1")
+//   })
+  
+//   let a=prompt("what is your fav num");
+//   if(a=="2"){
+//     btn1.removeEventListener('click',function(e){
+//       alert("hello")
+//     })
+//   }
